f58982e45fb837549b188f156b76b5a586b36ec1
DIYgod <i@diygod.me>
2021-11-09 18:45:39 +0000

    fix: types errors

--- lib/routes/name.ts
+++ lib/routes/name.ts
@@ -3,22 +3,22 @@ import RNS from '../utils/services/rns';
 import ENS from '../utils/services/ens';
 
 export default async (ctx: Koa.Context) => {
-    const name = ctx.params.name.replace(/\.rss3$/, '');
+    const name: string = ctx.params.name.replace(/\.rss3$/, '');
     const service = name.split('.')[1];
 
     let address;
-    let rnsName;
-    let ensName;
+    let rnsName: string | null = null;
+    let ensName: string | null = null;
     switch (service) {
         case undefined:
             rnsName = name;
             address = await RNS.name2Addr(rnsName);
-            ensName = await ENS.addr2Name(address);
+            address && (ensName = await ENS.addr2Name(address));
             break;
         case 'eth':
             ensName = name;
             address = await ENS.name2Addr(ensName);
-            rnsName = await RNS.addr2Name(address);
+            address && (rnsName = await RNS.addr2Name(address));
             break;
     }
 
